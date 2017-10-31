73d7a706f7cb3d4928a589967d4ee697ab9ffdbf
Lior Agnin <lior.agnin@gmail.com>
2017-10-31 20:47:00 +0200

    add neww applicant not done 2

--- Recruitment-Collaboration-Tool/src/app/components/add-new-applicant/add-new-applicant.component.ts
+++ Recruitment-Collaboration-Tool/src/app/components/add-new-applicant/add-new-applicant.component.ts
@@ -30,4 +30,23 @@ export class AddNewApplicantComponent implements OnInit {
     }
     console.log(this.arSkillSetPicked)
   }
+//   detectFiles(event) {
+//     this.selectedFiles = event.target.files;
+// }
+//   uploadSingle() {
+//     let file = this.selectedFiles.item(0)
+//     this.currentUpload = new Upload(file);
+//     this.upSvc.pushUpload(this.currentUpload)
+//   }
+
+//   uploadMulti() {
+//     let files = this.selectedFiles
+//     if (_.isEmpty(files)) return;
+
+//     let filesIndex = _.range(files.length)
+//     _.each(filesIndex, (idx) => {
+//       this.currentUpload = new Upload(files[idx]);
+//       this.upSvc.pushUpload(this.currentUpload)}
+//     )
+//   }
 }
