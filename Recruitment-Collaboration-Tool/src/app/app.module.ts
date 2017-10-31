077f66d1ae771b4b163d40bab7f3de35a45aca57
Lior Agnin <lior.agnin@gmail.com>
2017-10-31 11:58:06 +0200

    all components and models added

--- Recruitment-Collaboration-Tool/src/app/app.module.ts
+++ Recruitment-Collaboration-Tool/src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { ApplicantComponent } from './components/applicant/applicant.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddNewApplicantComponent } from './components/add-new-applicant/add-new-applicant.component';
+import { LoginComponent } from './components/login/login.component';
+import { AddNewJobComponent } from './components/add-new-job/add-new-job.component';
+import { EditJobComponent } from './components/edit-job/edit-job.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    JobsComponent,
+    ApplicantComponent,
+    HomeComponent,
+    AddNewApplicantComponent,
+    LoginComponent,
+    AddNewJobComponent,
+    EditJobComponent
   ],
   imports: [
     BrowserModule
