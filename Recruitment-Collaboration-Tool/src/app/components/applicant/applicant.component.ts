1a336bd8faef546b611451e84464003ec3698625
Mazal Gevyan <mazalg603@gmail.com>
2017-11-01 19:37:52 +0200

    update applicant

--- Recruitment-Collaboration-Tool/src/app/components/applicant/applicant.component.ts
+++ Recruitment-Collaboration-Tool/src/app/components/applicant/applicant.component.ts
@@ -9,28 +9,44 @@ import { Applicant } from "../../model/Applicant";
 })
 export class ApplicantComponent implements OnInit {
 
-  arAllApplicants:Applicant[]=new Array();
+  arAllApplicants: Applicant[] = new Array();
+  arApplicantStatus: Applicant[] = new Array();
+  arUnlockApplicants: Applicant[] = new Array();
 
   constructor(public ApplicantServiceService: ApplicantServiceService) { }
 
   ngOnInit() {
 
     console.log("ApplicantComponent");
-    this.ApplicantServiceService.getApplicants().subscribe(applicant=>{
-      this.arAllApplicants=applicant;
-      // console.log("applicant");
-      // console.log(this.arAllApplicants);
+
+    this.ApplicantServiceService.getApplicants().subscribe(applicant => {
+      this.arAllApplicants = applicant;
+      console.log(this.arAllApplicants);
     });
 
-    // this.ApplicantServiceService.getApplicantsStatus().subscribe(applicantStatus=>{
-    //   // console.log("applicantStatus");
-    //   // console.log(applicantStatus);
-    // });
+    this.ApplicantServiceService.getApplicantsStatus().subscribe(applicantStatus => {
+      this.arApplicantStatus = applicantStatus;
+      console.log(this.arApplicantStatus);
+    });
+
+    this.arAllApplicants.forEach(applicant => {
+      this.arApplicantStatus.forEach(applicantStatus => {
+
+        console.log(applicant);
+        console.log(applicantStatus);
+        // if(applicant.Id!= applicantStatus.ApplicantId)
+      });
+
+    });
 
     // this.ApplicantServiceService.getApplicantsHistory().subscribe(applicantHistory=>{
     //   // console.log("applicantHistory");
     //   // console.log(applicantHistory);
     // });
+
+
+
+    console.log("applicant");
   }
 
 }
--- Recruitment-Collaboration-Tool/src/app/services/skillset-service.service.ts
+++ Recruitment-Collaboration-Tool/src/app/services/skillset-service.service.ts
@@ -17,4 +17,6 @@ export class SkillsetServiceService {
     return this.skillsets;
   }
 
+  
+
 }
