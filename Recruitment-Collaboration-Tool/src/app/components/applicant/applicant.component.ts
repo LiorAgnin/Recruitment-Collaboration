55b3c226d649954347f7851fb00dace91d2ba424
weretaw <weretawt5@gmail.com>
2017-11-14 11:16:46 +0200

    commit lock for admin

--- Recruitment-Collaboration-Tool/src/app/components/applicant/applicant.component.ts
+++ Recruitment-Collaboration-Tool/src/app/components/applicant/applicant.component.ts
@@ -2,23 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { ApplicantServiceService } from "../../services/applicant-service.service";
 import { Applicant } from "../../model/Applicant";
 import { DataServiceService } from "../../services/data-service.service";
-
+import { Manger } from "../../model/manger";
+import { AngularFireAuth } from 'angularfire2/auth';
 @Component({
   selector: 'app-applicant',
   templateUrl: './applicant.component.html',
-  styleUrls: ['./applicant.component.css']
+  styleUrls: ['./applicant.component.css'],
+  providers: [AngularFireAuth]
 })
 export class ApplicantComponent implements OnInit {
 
   arAllApplicants: Applicant[] = new Array();
   arApplicantStatus: Applicant[] = new Array();
   arUnlockApplicants: Applicant[] = new Array();
+  manger:string;
   LockUnlock: boolean = false;
   constructor(public applicantService: ApplicantServiceService,
-    public dataService: DataServiceService) { }
+    public dataService: DataServiceService,private auth:AngularFireAuth) { }
 
   ngOnInit() {
-
+   
     console.log("ApplicantComponent");
 
     this.applicantService.getApplicants().subscribe(applicant => {
@@ -53,9 +56,17 @@ export class ApplicantComponent implements OnInit {
 
   }
 
-  lockToggle(applicant: Applicant) {
-    applicant.IsActive = !applicant.IsActive;
-    this.applicantService.updeteApplicants(applicant);
+  lockToggle(applicant: Applicant,manger:Manger) {
+
+    console.log("My email",this.auth.auth.currentUser.email)
+
+    if(this.auth.auth.currentUser.email=='weretawt5@gmail.com')
+    {
+      applicant.IsActive = !applicant.IsActive;
+      this.applicantService.updeteApplicants(applicant);
+      this.manger='weretawt5@gmail.com';
+    }
+    
 
   }
 
